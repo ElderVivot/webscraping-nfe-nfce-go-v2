b7bfea981c5bf5d360da828d116b3fbb1117c5e7
Elder Vivot Dias <elder.dias@unicontabil.local>
2022-11-02 17:29:26 -0300

    feat: implement to close node.exe when cmd webscraping-nfe-nfce-go-v2 is folder start

--- src/schedules/app.ts
+++ src/schedules/app.ts
@@ -1,9 +1,26 @@
 import express from 'express'
+import findProcess from 'find-process'
+import { kill } from 'process'
 
 import { job00 /*, job09, job16 */ } from './jobs/NFeNFCeGO'
 import { jobError, jobProcessing, jobToProcess, jobWarning } from './jobs/NFeNFCeGOReprocess'
 
 const app = express()
+
+async function closeNodeExe () {
+    try {
+        const processes = await findProcess('name', 'node', true)
+        for (const proc of processes) {
+            try {
+                if (proc.cmd.indexOf('webscraping-nfe-nfce-go-v2') >= 0) {
+                    kill(proc.ppid)
+                }
+            } catch (error) { }
+        }
+    } catch (error) { }
+}
+
+closeNodeExe().then(_ => console.log())
 
 async function process () {
     job00.start()
