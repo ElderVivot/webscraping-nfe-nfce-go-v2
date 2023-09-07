a88f2ecff633e68a73581d520017aa70f2901b1e
Elder Vivot <eldervivot@gmail.com>
2023-09-07 10:31:05 -0300

    feat: implement to option delete all certificate

--- src/services/certificates/windows/prepare-certificate-regedit.ts
+++ src/services/certificates/windows/prepare-certificate-regedit.ts
@@ -3,16 +3,23 @@ import path from 'path'
 import { logger } from '@common/log'
 import { ISettingsNFeGoias } from '@scrapings/_interfaces'
 
-// import { mainDeleteCertificates } from './delete-certificates'
+import { mainDeleteCertificates } from './delete-certificates'
 import { installCertificate } from './install-certificates'
 import { mainSetDefaultCertificateRegedit } from './set-default-certificate-regedit'
+
+const { DELETE_CERTIFICATE_ALL_PROCESSING } = process.env
+
+let deleteCertificateAllProcessing = false
+if (DELETE_CERTIFICATE_ALL_PROCESSING && DELETE_CERTIFICATE_ALL_PROCESSING === '1') deleteCertificateAllProcessing = true
 
 export async function prepareCertificateRegedit (fileCertificate: string, settings: ISettingsNFeGoias): Promise<void> {
     try {
         const nameFile = path.basename(fileCertificate)
 
-        // logger.info('- Deletando certificados')
-        // await mainDeleteCertificates(true)
+        if (deleteCertificateAllProcessing) {
+            logger.info('- Deletando certificados')
+            await mainDeleteCertificates()
+        }
 
         logger.info(`- Instalando certificado ${nameFile}`)
         await installCertificate(fileCertificate, settings.passwordCert)
